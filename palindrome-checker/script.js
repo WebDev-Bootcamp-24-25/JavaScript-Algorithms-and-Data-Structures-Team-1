8c769fed97c4cc86517394a0f06a642fb8d58799
arcanine31 <arcanine215@gmail.com>
2024-11-23 18:06:19 +0700

    adding keyboard enter capability to checking palindrome

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -2,19 +2,33 @@ const textInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
-checkButton.addEventListener("click", () => {
-    const LowerReplaced = textInput.value.toLowerCase().replace(/[^A-Za-z0-9]/g, "");
+const palindromeCheck = input =>{
 
-    if (textInput.value === "") {
+    const LowerReplaced = input.toLowerCase().replace(/[^A-Za-z0-9]/g, "");
+
+    if (input === "") {
         alert("Please input a value");
-    } else if (textInput.value.length === 1) {
-        displayResult(`${textInput.value} is a Palindrome`);
+    } else if (input.length === 1) {
+        displayResult(`${input} is a Palindrome`);
     } else if (LowerReplaced === [...LowerReplaced].reverse().join("")) {
-        displayResult(`${textInput.value} is a Palindrome`);
+        displayResult(`${input} is a Palindrome`);
     } else {
-        displayResult(`${textInput.value} is not a Palindrome`);
+        displayResult(`${input} is not a Palindrome`);
     }
+}
+
+checkButton.addEventListener("click", () => {
+    palindromeCheck(textInput.value);
+    userInput.value = '';
 });
+
+textInput.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        palindromeCheck(textInput.value);
+        userInput.value = '';
+    }
+  });
+
 
 function displayResult(resultText) {
     result.innerHTML = ''; 
